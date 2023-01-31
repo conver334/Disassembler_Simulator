28c7c8d04d863613eb73618d4e741ca40f224853
conver <conver334@gmail.com>
2023-01-31 15:52:50 +0800

    test

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 g++ main.cpp -o MIPSsim
-./MIPSsim sample_test.txt simulation.txt
-diff -cb simulation.txt simulation_test.txt >diff.txt
-diff -cb disassembly.txt disassembly_test.txt >diff2.txt
+./MIPSsim t1.txt simulation.txt
+diff -cb simulation.txt t1_sim.txt >diff.txt
+diff -cb disassembly.txt t1_dis.txt >diff2.txt
