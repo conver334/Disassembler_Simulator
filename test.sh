8fe9fbc733aba9e83fde58aeab83e1d14b8b468b
conver <conver334@gmail.com>
2023-01-31 15:49:47 +0800

    test

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 g++ main.cpp -o main
-./main sample_test.txt
-diff -cb simulation.txt simulation_test.txt >diff.txt
-diff -cb disassembly.txt disassembly_test.txt >diff2.txt
+./main sample_int.txt
+diff -cb simulation.txt simulation_int.txt >diff.txt
+diff -cb disassembly.txt disassembly_int.txt >diff2.txt
